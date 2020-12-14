4f78401d98c051b2064520c8068f4b7bca8d9785
Umme-hanii <53809131+Umme-hanii@users.noreply.github.com>
2020-12-14 22:33:50 +0530

    setup mongoose and User Schema

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+//require library
+const mongoose = require('mongoose');
+//connect to the database
+mongoose.connect('mongodb://localhost/codeial_development');
+//acquire the connection (to check if it's successful)
+const db = mongoose.connection;
+//If there is error , print error
+db.on('error', console.error.bind('console', 'Connection error'));
+//If connection is established
+db.once('open', function() {
+    console.log("Connected to database!!!");
+});
+
+module.exports = db;
