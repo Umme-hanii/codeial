34287c27cd4d4898f497dc9408b8104f43872bbf
Umme-hanii <53809131+Umme-hanii@users.noreply.github.com>
2020-12-16 18:43:02 +0530

    create user(sign up)

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.get('/profile', usersController.profile);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
+router.post('/create', usersController.create);
 //if user is not new
 // router.get('/sign-in-user', usersController.existing);
 //login
