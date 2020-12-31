0dea8e30dda3131ce825deaf99ebae9daa8e5aae
Umme-hanii <53809131+Umme-hanii@users.noreply.github.com>
2020-12-31 20:21:35 +0530

    Nested population: Display comment and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.home = function(req, res) {
    // console.log(req.cookies);
@@ -11,11 +12,27 @@ module.exports.home = function(req, res) {
    // });
 
    // Populate the user of each post
-   Post.find({}).populate('user').exec(function(err, posts) {
+   Post.find({})
+   .populate('user')
+   .populate({
+      path: 'comments',
+      populate: {
+         path: 'user'
+      }
+   })
+   .exec(function(err, posts) {
       return res.render('home', {
            title: 'Codeial | Home',
            posts: posts
       });
    });
+   
+   // Populate the comment of each post
+   // Comment.find({}).populate('user').populate('post').exec(function(err, comments) {
+   //    return res.render('home', {
+   //       title: 'Codeial | Home',
+   //       comments: comments
+   //    });
+   // });
    
 }
