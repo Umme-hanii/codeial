808c61f88a58388350cd5670d994c09a837a9032
Umme-hanii <53809131+Umme-hanii@users.noreply.github.com>
2020-12-31 13:24:24 +0530

    Add Comments to the DB

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const postsController = require('../controllers/posts_controller');
+const commentsController = require('../controllers/comments_controller');
 
 // router.get('/post', postsController.post);
 
-router.post('/create', passport.checkAuthentication, postsController.create);
+router.post('/create', passport.checkAuthentication, commentsController.create);
 
 module.exports = router;
