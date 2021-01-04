a3d30d3450b0445fd67c2c06952b48b707e643ff
Umme-hanii <53809131+Umme-hanii@users.noreply.github.com>
2021-01-04 06:53:50 +0530

    Improve SCSS of home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,14 +3,6 @@ const Comment = require('../models/comment');
 const User = require('../models/user');
 
 module.exports.home = function(req, res) {
-   // console.log(req.cookies);
-   // res.cookie('user_id', 789);
-   // Post.find({}, function(err, posts) {
-   //    return res.render('home', {
-   //       title: 'Codeial | Home',
-   //       posts: posts
-   //    });
-   // });
 
    // Populate the user of each post
    Post.find({})
