19a3572e0663e6145bb3897c3bb3aa34f6e211f3
Umme-hanii <53809131+Umme-hanii@users.noreply.github.com>
2021-01-04 20:03:10 +0530

    Create flash messages

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 app.use(sassMiddleware({
     src: './assets/scss',
@@ -63,6 +65,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+//using connect-flash
+app.use(flash());
+app.use(customMware.setFlash);
+
 //use express router
 app.use('/', require('./routes'));
 
